73954c27ccc998b0842b0a02a9359ad307827c89
Cícero Alves <jalves3003@gmail.com>
2026-05-12 22:39:56 -0300

    feat(App.js): Importação da tela splash e inclusão da mesma na função App()

--- frontend/App.js
+++ frontend/App.js
@@ -4,22 +4,38 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 
 import { AuthProvider } from './src/contexts/AuthContext';
+
+// Importação das Telas
+import SplashScreen from './src/screens/SplashScreen';
 import LoginScreen from './src/screens/LoginScreen';
 
-// Proximas telas que forem criadas seram adicionadas aqui:
-// RegisterScreen
-// HomeScreen
-
 const Stack = createNativeStackNavigator();
+
+// Placeholder para não dar erro enquanto não criar o arquivo
+function CadastroPlaceholder() {
+    return null;
+}
 
 export default function App() {
     return (
         <AuthProvider>
             <NavigationContainer>
                 <StatusBar style = "light"/>
-                <Stack.Navigator screenOptions={{ headerShown : false}}>
-                    <Stack.Screen name = "Login" component = {LoginScreen}/>
-                    {/* Adiciona as proximas telas aqui */}
+
+                <Stack.Navigator
+                    initialRouteName = "Splash"
+                    screenOptions = {{ headerShown: false}}
+                >
+
+                    {/* tela de entrada Splash */}
+                    <Stack.Screen name = "Splash" component = {SplashScreen} />
+
+                    {/* Tela de login */}
+                    <Stack.Screen name = "Login" component = {LoginScreen} />
+
+                    {/* Proximas telas */}
+                    <Stack.Screen name = "Cadastro" component = {CadastroPlaceholder} />
+                    
                 </Stack.Navigator>
             </NavigationContainer>
         </AuthProvider>
