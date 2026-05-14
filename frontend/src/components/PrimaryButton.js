931bcb2221fa5942f3c6fe4699313beeb1ef92e9
Cícero Alves <jalves3003@gmail.com>
2026-05-14 10:25:35 -0300

    feat(PrimaryButton.js): elaboração de botão presente na tela de Login e Cadastro. Botão (Iniciar)

---
+++ frontend/src/components/PrimaryButton.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { TouchableOpacity, Text, SyleSheet } from 'react-native';
+import { colors, typography, spacing, radius} from '../theme';
+
+export default function PrimaryButton({ title, onPress, style}) {
+    return (
+        <TouchableOpacity
+            style = {[styles.button, style]}
+            onPress = {onPress}
+            activeOpacity = {0.8}
+        >
+            <Text style = {styles.text}>{title}</Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: colors.primary,
+        with: '100%',
+        paddingVertical: spacing.md,
+        borderRadius: radius.md,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: spacing.md,
+    },
+    text: {
+        ...typography.h2,
+        color: colors.white,
+        frontWeight: 'bold',
+    }
+});
