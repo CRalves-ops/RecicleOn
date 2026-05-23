943dd3bd406878b7dbae5eb12cf10df6e3905790
Cícero Alves <jalves3003@gmail.com>
2026-05-23 14:40:28 -0300

    fix(server.js): alteração da variação rota, que estava errada

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
-const authRoutes = require('./routes/auth.routes');
+const router = require('./routes/auth.routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Rotas
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', router);
 
 // Health check
 app.get('/api/health', (req, res) => {
@@ -22,6 +22,6 @@ app.get('/api/health', (req, res) => {
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(' RecicleON API rodando em http://localhost:${PORT}');
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`RecicleON API rodando em http://localhost:${PORT}`);
 });
