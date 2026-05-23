a31c5c690bf687e5d6f68f04da8c2e4d31c58ee8
Cícero Alves <jalves3003@gmail.com>
2026-05-23 15:46:26 -0300

    feat(HeaderLogo.js): Criado cabeçalho presente nas telas da HOME

---
+++ frontend/src/components/HeaderLogo.js
@@ -0,0 +1,34 @@
+import react from "react";
+import {
+    View,
+    Image,
+    StyleSheet
+} from 'react-native';
+import { colors, spacing } from "../theme";
+
+export default function HeaderLogo() {
+    return (
+        <View style = {styles.container}>
+            <Image
+                source = {require('../components/logos/logo_verde.png')}
+                style = {styles.logo}
+                resizeMode = "contain"
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container : {
+        backgroundColor: colors.primaryBorder,
+        width: '100%',
+        alignItems : 'center',
+        paddingVertical : spacing.md,
+        borderBottomWidth : 1,
+        borderBottomColor: colors.primaryLight,
+    },
+    logo : {
+        width: 120,
+        height: 60,
+    }
+});
